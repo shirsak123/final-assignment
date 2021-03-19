66f1354b474d545acdb1457ec9c77e4f3c27a3ee
shirsak123 <shirsak.shrestha@gmail.com>
2021-03-19 22:19:59 +0545

    this is my final assignment

---
+++ DYC_System_Management/DYC_System_Management/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for DYC_System_Management project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'DYC_System_Management.settings')
+
+application = get_wsgi_application()
