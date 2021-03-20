b3dd6402d6efec8a8d8752f2afda3143dfb83362
shirsak123 <shirsak.shrestha@gmail.com>
2021-03-20 21:15:37 +0545

    Adding new app

--- DYC_System_Management/DYC_System_Management/settings.py
+++ DYC_System_Management/DYC_System_Management/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'DYC_System_Management.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR,'templates')],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -77,7 +77,7 @@ WSGI_APPLICATION = 'DYC_System_Management.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
